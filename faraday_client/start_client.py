37b40db625ba154c761b7e4571ef20170686d7d9
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2020-05-12 19:05:45 -0300

    simplify login logic

--- faraday_client/start_client.py
+++ faraday_client/start_client.py
@@ -371,88 +371,65 @@ def doLoginLoop(force_login=False, use_2fa=False):
     Sets the username and passwords from the command line.
     If --login flag is set then username and password is set
     """
-
+    CONF = getInstanceConfiguration()
     try:
-        CONF = getInstanceConfiguration()
         old_server_url = CONF.getAPIUrl()
+        if force_login:
+            if old_server_url is None:
+                server_url = input("\nPlease enter the Faraday Server URL (Press enter for http://localhost:5985): ") \
+                                 or "http://localhost:5985"
+            else:
+                server_url = input(f"\nPlease enter the Faraday Server URL (Press enter for last used: {old_server_url}): ")\
+                                 or old_server_url
+        else:
+            if not old_server_url:
+                server_url = input("\nPlease enter the Faraday Server URL (Press enter for http://localhost:5985): ") \
+                             or "http://localhost:5985"
+            else:
+                server_url = old_server_url
+        CONF.setAPIUrl(server_url)
+        if force_login:
+            print("""\nTo login please provide your valid Faraday credentials.\nYou have 3 attempts.""")
         api_username = CONF.getAPIUsername()
         api_password = CONF.getAPIPassword()
-        if old_server_url and api_username and api_password and not force_login:
-            return
-
-        if old_server_url is None:
-            new_server_url = input(
-            "\nPlease enter the Faraday Server URL (Press enter for http://localhost:5985): ") or "http://localhost:5985"
-        else:
-            new_server_url = input(
-                "\nPlease enter the Faraday Server URL (Press enter for last used: {}): ".format(old_server_url)) or old_server_url
-
-        CONF.setAPIUrl(new_server_url)
-
-        print("""\nTo login please provide your valid Faraday credentials.\nYou have 3 attempts.""")
-
         for attempt in range(1, 4):
-
-            api_username = input("Username (press enter for faraday): ") or "faraday"
-            api_password = getpass.getpass('Password: ')
+            if force_login or (not api_username or not api_password):
+                api_username = input("Username (press enter for faraday): ") or "faraday"
+                api_password = getpass.getpass('Password: ')
             if use_2fa:
                 api_2fa = input("2FA Token: ")
             else:
                 api_2fa = None
-            session_cookie = try_login_user(new_server_url, api_username, api_password, api_2fa)
-
+            session_cookie = try_login_user(server_url, api_username, api_password, api_2fa)
             if session_cookie:
-
                 CONF.setAPIUsername(api_username)
                 CONF.setAPIPassword(api_password)
                 CONF.setDBSessionCookies(session_cookie)
                 CONF.saveConfig()
-
                 user_info = get_user_info()
                 if not user_info:
                     continue
                 else:
                     if 'roles' in user_info:
                         if 'client' in user_info['roles']:
-                            print("You can't login as a client. You have %s attempt(s) left." % (3 - attempt))
-                            continue
+                            if force_login:
+                                print("You can't login as a client. You have %s attempt(s) left." % (3 - attempt))
+                                continue
+                            else:
+                                print("You can't login as a client.")
+                                sys.exit(-1)
                     logger.info('Login successful: {0}'.format(api_username))
                     break
             print('Login failed, please try again. You have %d more attempts' % (3 - attempt))
-
         else:
             logger.fatal('Invalid credentials, 3 attempts failed. Quitting Faraday...')
             sys.exit(-1)
-
     except KeyboardInterrupt:
         sys.exit(0)
 
 
 def login(forced_login, use_2fa):
-    CONF = getInstanceConfiguration()
-    server_uri = CONF.getServerURI()
-    api_username = CONF.getAPIUsername()
-    api_password = CONF.getAPIPassword()
-
-    if forced_login:
-        doLoginLoop(forced_login, use_2fa)
-        return
-    else:
-        if use_2fa:
-            api_2fa = input("2FA Token: ")
-        else:
-            api_2fa = None
-
-    if server_uri and api_username and api_password:
-
-        session_cookie = try_login_user(server_uri, api_username, api_password, api_2fa)
-
-        if session_cookie:
-            CONF.setDBSessionCookies(session_cookie)
-            logger.info('Login successful: {0}'.format(api_username))
-            return
-
-    doLoginLoop()
+    doLoginLoop(forced_login, use_2fa)
 
 
 def main():
